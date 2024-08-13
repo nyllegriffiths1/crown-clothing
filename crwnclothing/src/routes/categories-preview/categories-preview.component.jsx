0d70b5c998611136822ffaf53516d1eaaec4b99c
nyllegriffiths1 <nyllegriffiths1@googlemail.com>
2024-08-13 23:37:00 +0100

    need to fix styling errors

---
+++ crwnclothing/src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+
+
+import { CategoriesContext } from '../../context/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component.jsx';
+import './categories-preview.styles.scss';
+
+
+const CategoriesPreview = () => {
+
+    const { categoriesMap } = useContext(CategoriesContext);
+
+    return (
+        <div className="category-provider-container">
+            {Object.keys(categoriesMap).map((title) => {
+               const products = categoriesMap[title];
+               return (
+                <CategoryPreview key={title} title={title} products={products} />
+               );
+            })}
+        </div>
+    );
+};
+
+export default CategoriesPreview;
