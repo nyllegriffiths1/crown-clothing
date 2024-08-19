f8c6012273be26ac89468121f9e003a6d82bfbd1
nyllegriffiths1 <nyllegriffiths1@googlemail.com>
2024-08-19 14:03:36 +0100

    frontend and database finished, need to add redux for state managament

--- crwnclothing/src/components/category-menu/category-menu.component.jsx
+++ crwnclothing/src/components/category-menu/category-menu.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 import './categories.styles.scss';
 
 const CategoryMenu = () => {
@@ -33,7 +33,7 @@ const CategoryMenu = () => {
     return (
         <div className="categories-container">
             {categories.map((category) => (
-            <CategoryItem key={category.id} category={category}/>
+            <DirectoryItem key={category.id} category={category}/>
             ))}
         </div>
     );
